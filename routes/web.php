5f4c59779921e107312e9c24845eeeb026a2f0d2
Marcela Malaver <marcela9409@gmail.com>
2018-02-09 10:58:25 -0500

    Routing: Creacion de rutas en routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//http://localhost/blog/public/articles
+Route::get('articles', function () {
+    echo "This is article's section";
+});
+
+//http://localhost/blog/public/articles/nombreAqui
+Route::get('articles/{nombre}', function ($nombre) {
+    echo "This is article's section with name: ".$nombre;
+});
+
+Route::group(['prefix'=>'articles1'], function () {
+    //http://localhost/blog/public/articles1/view/
+    //http://localhost/blog/public/articles1/view/articuloAqui
+    Route::get('view/{article?}', function ($article="vacio") {
+        echo $article;
+    });
+});
